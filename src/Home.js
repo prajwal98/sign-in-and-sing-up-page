ab0d2fb834ec053a2661d9e8872a9797b8b9b8ff
Dev <ursprajwal1@gmail.com>
2020-10-26 16:18:13 +0530

    Home page is updated with styles and custom button

--- src/Home.js
+++ src/Home.js
@@ -19,9 +19,26 @@ class Home extends React.Component {
     render(){
         return(
            
-               <div>
-                    <h1>You are logged In</h1>
-                     <button onClick={this.logout}>Logout</button>
+               <div className='section-book' >
+                    <div className='row'>
+                        <div className='book'>
+                        <div className='book__form'>
+                            <form className='form'>
+                            <div className="u-margin-bottom-medium">
+                                    <h2 className="heading-secondary">
+                                    You are logged In
+                                    </h2>
+                                   
+                                </div>
+
+                                <button className='btn btn--green' onClick={this.logout}>Logout</button>
+                                </form>
+                                </div>
+                                
+                        </div>
+                    </div>
+                   
+                    
                   
                </div>    
 
